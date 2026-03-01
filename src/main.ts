6b81c7f2938fea70babecc44cba86dadc9f62ee4
ITAQ <it@alphaquimicasrl.com.ar>
2026-03-01 20:58:12 -0300

    agg ./api a routas de main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api');
+
   // Habilitamos CORS para que Vercel pueda conectarse
   app.enableCors({
     origin: '*',
