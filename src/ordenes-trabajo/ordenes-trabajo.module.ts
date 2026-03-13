d789c46b3ff01f6f2e7e1b9cd646288ed78689ff
ITAQ <it@alphaquimicasrl.com.ar>
2026-03-13 08:20:25 -0300

    cambio en repository y dto en ordenes de trabajo

--- src/ordenes-trabajo/ordenes-trabajo.module.ts
+++ src/ordenes-trabajo/ordenes-trabajo.module.ts
@@ -3,10 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrdenesTrabajoController } from './ordenes-trabajo.controller';
 import { OrdenesTrabajoService } from './ordenes-trabajo.service';
 import { OrdenTrabajo } from './entities/orden-trabajo.entity';
+import { OrdenesTrabajoRepository } from './ordenes-trabajo.repository'; // 1. Asegúrate de importarlo
 
 @Module({
   imports: [TypeOrmModule.forFeature([OrdenTrabajo])],
   controllers: [OrdenesTrabajoController],
-  providers: [OrdenesTrabajoService],
+  providers: [
+    OrdenesTrabajoService, 
+    OrdenesTrabajoRepository // 2. Agregalo aquí para que Nest lo pueda inyectar
+  ],
 })
 export class OrdenesTrabajoModule {}
